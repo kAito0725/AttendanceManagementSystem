98d8dd221b572fdde081925a3f7867af5061eb0b
kAito0725 <kaitoaoki0725@gmail.com>
2021-07-31 21:50:19 +0900

    Add tabs for home component

---
+++ src/Components/Tabs/Tabs.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import TimeCards from '../TimeCards/TimeCards';
+import Calendars from '../Calendars/Calendars';
+import './Tabs.css';
+
+export default () => (
+  <>
+    <Tabs>
+      <TabList>
+        <Tab>Attendance TimeCards</Tab>
+        <Tab>Calendar</Tab>
+      </TabList>
+
+      <TabPanel>
+        <div className="Position">
+          <h3>Director</h3>
+        </div >
+        <div className="Card">
+          <TimeCards name="Test user" />
+          <TimeCards name="Test user" />
+        </div>
+      </TabPanel>
+
+      <TabPanel>
+        <Calendars />
+      </TabPanel>
+    </Tabs>
+  </>
+);
