b0566923e977581ae3f7978bf3c227584f44fc24
kAito <kaitoaoki0725@gmail.com>
2021-08-06 21:58:55 +0900

    Delete import React from 'react' by JSX transform improved from React 17

--- src/Components/TimeCards/TimeCards.js
+++ src/Components/TimeCards/TimeCards.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import Button from '@material-ui/core/Button';
 import "./TimeCards.css";
